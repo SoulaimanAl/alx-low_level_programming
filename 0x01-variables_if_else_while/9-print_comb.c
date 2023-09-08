0493347d9ad45fd6ee4e205574d540f8c35b289a
alahiane <ahmedpzzz24@gmail.com>
2023-09-08 03:00:59 -0700

    azer

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		putchar(c);
-		if (c == '9')
+		putchar(c + '0');
+		if (c == 9)
 			break;
 		putchar(',');
 		putchar(' ');
