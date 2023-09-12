a3e4a39222e840fa349d222c05a27b206d57364a
alahiane <ahmedpzzz24@gmail.com>
2023-09-12 14:28:26 -0700

    azer

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,20 +9,30 @@
 
 int main(void)
 {
-	unsigned long int f = 1, s = 2, n;
-	int count = 2;
+	unsigned long int i, f1, f2, s1, s2, f = 1, s = 2, max = 1000000000;
 
-	printf("1, 2, ");
-	while (count < 98)
+	printf("%lu", f);
+
+	for (i = 1; i < 91; i++)
 	{
-		n = f + s;
-		if (count == 97)
-			printf("%lu", n);
-		else
-			printf("%lu, ", n);
-		f = s;
-		s = n;
-		count++;
+		printf(", %lu", s);
+		s += f;
+		f = s - f;
+	}
+
+	f1 = (f / max);
+	f2 = (f % max);
+	s1 = (s / max);
+	s2 = (s % max);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", s1 + (s2 / max));
+		printf("%lu", s2 % max);
+		s1 = s1 + f1;
+		f1 = s1 - f1;
+		s2 = s2 + f2;
+		f2 = s2 - f2;
 	}
 	printf("\n");
 	return (0);
