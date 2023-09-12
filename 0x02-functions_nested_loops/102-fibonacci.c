674aaf6cdabf9d74dfcbae9ff30e67fb2d6d67fc
alahiane <ahmedpzzz24@gmail.com>
2023-09-12 12:46:10 -0700

    azer

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-	long long int f = 1, s = 2, n;
+	long int f = 1, s = 2, n;
 	unsigned int count = 2;
 
 	printf("1, 2,");
 	while (count < 50)
 {
 		n = f + s;
-		printf("%lld, ", n);
+		printf("%ld, ", n);
 		f = s;
 		s = n;
 		count++;
