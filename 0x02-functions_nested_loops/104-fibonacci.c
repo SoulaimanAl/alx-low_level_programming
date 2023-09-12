b364c3dbfdf74142292823eadd7295d2ce8ece6a
alahiane <ahmedpzzz24@gmail.com>
2023-09-12 13:54:47 -0700

    azer

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - Retry point
+ *
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	unsigned long int f = 1, s = 2, n;
+	int count = 2;
+
+	printf("1, 2, ");
+	while (count < 98)
+	{
+		n = f + s;
+		if (count == 97)
+			printf("%ld", n);
+		else
+			printf("%ld, ", n);
+		f = s;
+		s = n;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
