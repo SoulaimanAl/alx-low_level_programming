9679223dafd7f9da5c2b62ecccbdce3caaedbef1
alahiane <ahmedpzzz24@gmail.com>
2023-09-12 13:08:07 -0700

    azer

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Retry point
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	unsigned long int f = 1, s = 2, n;
+	long int sum = 0;
+
+	while (s < 4000000)
+	{
+		if (s % 2 == 0)
+			sum = sum + s;
+		n = f + s;
+		f = s;
+		s = n;
+	}
+	printf("%ld \n", sum);
+	return (0);
+}
