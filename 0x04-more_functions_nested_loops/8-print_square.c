21b825ebbec8db815244af3a350caa0394bf9f5b
alahiane <ahmedpzzz24@gmail.com>
2023-09-14 04:50:14 -0700

    azer

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints a square
+ *
+ * @size:  is the size of the square
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 0; a <= size; a++)
+		{
+			for (b = 0; b <= size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
