5f60fd88b1ddc570378a22f659a4f00f6f36f414
alahiane <ahmedpzzz24@gmail.com>
2023-09-14 07:25:58 -0700

    azer

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * main - Retry point
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
  *
  * Return: Always 0
  */
