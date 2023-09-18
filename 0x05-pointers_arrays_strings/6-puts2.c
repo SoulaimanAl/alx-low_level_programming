4e0e1cce3dcb42fe845e3d86c3acb5df5e99aef9
alahiane <ahmedpzzz24@gmail.com>
2023-09-18 07:14:52 -0700

    azer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ *
+ * @str: pointeur type char
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
