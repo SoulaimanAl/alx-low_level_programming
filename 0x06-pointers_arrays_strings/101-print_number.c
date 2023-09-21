db08ef3ea5a80a820958d981b1e7b05ae3c8f3ea
alahiane <ahmedpzzz24@gmail.com>
2023-09-21 08:25:31 -0700

    azer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,13 +16,14 @@ void print_number(int n)
 	{
 		num = -n;
 		_putchar('-');
-	}
-	else
+	} else
+	{
 		num = n;
+	}
 
 	if (num / 10)
 	{
 		print_number(num / 10);
 	}
-	_putchar((num % 10)) + '0');
+	_putchar((num % 10) + '0');
 }
