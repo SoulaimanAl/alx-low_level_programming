93615ff6c0f87e0946b8a6510af8f48d3a1b0b97
alahiane <ahmedpzzz24@gmail.com>
2023-09-25 12:40:07 -0700

    azer

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,24 +10,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (; *haystack != '\0'; haystack++)
 	{
-		if (haystack[i] == needle[0])
+		char *b = haystack;
+		char *a = needle;
+
+		while (*b == *a && *a != '\0')
 		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-				{
-					break;
-				}
-			}
-			if (needle[j] == '\0')
-			{
-				return (&haystack[i]);
-			}
+			b++;
+			a++;
 		}
+
+		if (*a == '\0')
+			return (haystack);
 	}
-	return (NULL);
+
+	return (0);
 }
