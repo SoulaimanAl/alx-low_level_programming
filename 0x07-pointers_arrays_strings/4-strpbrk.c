214ada32ceaf50d25f9f564c865a826fbf66e473
alahiane <ahmedpzzz24@gmail.com>
2023-09-25 12:22:13 -0700

    azer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,19 +11,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int j = 0;
+	int j;
 
 	while (*s)
 	{
-		while (accept[j])
+		for (j = 0; accept[j]; j++)
 		{
 			if (*s == accept[j])
-			{
-				return (s);
-			}
-			j++;
+			return (s);
 		}
 		s++;
 	}
 	return ('\0');
+
 }
