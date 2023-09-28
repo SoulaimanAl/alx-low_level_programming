b2de329ba25b49a95e75047527accb2fbb8038f3
alahiane <ahmedpzzz24@gmail.com>
2023-09-28 12:34:37 -0700

    azer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Entry point
+ *
+ * @n: input
+ * @x: the current divisor
+ *
+ * Return: always 0
+ */
+
+int _prime(int n, int x);
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (_prime(n, n - 1));
+}
+
+/**
+ * _prime - Entry point
+ *
+ * @n: input
+ * @x: the current divisor
+ *
+ * Return: always 0
+ */
+int _prime(int n, int x)
+{
+	if (x == 1)
+		return (1);
+	else if (n % x == 0 && x > 0)
+		return (0);
+	return (_prime(n, x - 1));
+}
