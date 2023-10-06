b60e9c0e2db8e1d3bde2c8618591b3064fe961e6
alahiane <ahmedpzzz24@gmail.com>
2023-10-06 02:39:29 -0700

    azer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - Entry point
+ *
+ * @b: input
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *n;
+
+	n = malloc(sizeof(int) * b);
+	if (n == 0)
+		return (98);
+	return (n);
+}
