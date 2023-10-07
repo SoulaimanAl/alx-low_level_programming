c62ab1abe135f86a8d6f088f34ffac3326098585
alahiane <ahmedpzzz24@gmail.com>
2023-10-07 03:27:42 -0700

    azer

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,8 +34,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < l1; i++)
 		str[i] = s1[i];
 
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n; j++)
+	{
 		str[i] = s2[j];
+		i++;
+	}
 
 	str[i] = '\0';
 	return (str);
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *n;
+	int *n;
 	int b = (size * nmemb);
 
 	if (nmemb == 0 || size == 0)
