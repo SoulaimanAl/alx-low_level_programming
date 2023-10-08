ec5abea2cb2af1c7855d712a9994551ff627b2a9
alahiane <ahmedpzzz24@gmail.com>
2023-10-08 06:29:29 -0700

    azer

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,9 +6,46 @@
  * @ptr: input
  * @old_size: input
  * @new_size: input
+ *
+ * Return: pointer to the newly allocated memory block
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *p1;
+	char *p2;
+	int i;
 
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	p1 = malloc(new_size);
+	if (!p1)
+		return (NULL);
+
+	p2 = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			p1[i] = p2[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p1[i] = p2[i];
+	}
+
+	free(ptr);
+	return (p1);
 }
