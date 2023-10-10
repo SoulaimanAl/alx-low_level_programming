d843951e7559d7310a6b7ff83f718ee8b390207f
alahiane <ahmedpzzz24@gmail.com>
2023-10-09 23:49:41 -0700

    azer

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJET_LIKE_MACRO_H
+#define OBJET_LIKE_MACRO_H
+
+#define size 1024
+
+#endif /*OBJET_LIKE_MACRO_H*/
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Retry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __file__);
+	return (0);
+}
