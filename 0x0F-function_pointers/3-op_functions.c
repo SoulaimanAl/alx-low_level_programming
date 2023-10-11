b75637936fded1d1ead669c10408be994c6a7c21
alahiane <ahmedpzzz24@gmail.com>
2023-10-11 13:59:58 -0700

    azer

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "caloc.h"
+
+/**
+ * op_add - Entry point
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Entry point
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Entry point
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Entry point
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Entry point
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
