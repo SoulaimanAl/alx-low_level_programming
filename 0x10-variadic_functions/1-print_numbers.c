22f5e9a648a355697103ebdd956ecd3094252eb7
alahiane <ahmedpzzz24@gmail.com>
2023-10-12 13:32:42 -0700

    azer

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * @separator: input
+ * @n: input
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+	va_list num;
+
+	if (separator == NULL)
+		printf("\n"), return;
+	va_start(num, n);
+	while (i < n)
+	{
+		if (i == n - 1)
+			printf("%d", va_arg(num, int));
+		else
+			printf("%d%s", va_arg(num, int), *separator);
+		i++;
+	}
+	printf("\n");
+	va_end(num);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Entry point
+ *
+ * @separator: input
+ * @n: input
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+	va_list s;
+
+	if (separator == NULL)
+		printf("\n"), return;
+
+	va_start(s, n);
+	while (i < n)
+	{
+		if (s == NULL)
+			printf("(nil)");
+		if (i == n - 1)
+			printf("%s", va_arg(s, char));
+		else
+			printf("%s%s", va_arg(s, char), *separator);
+		i++;
+	}
+	printf("\n");
+	va_end(s);
+}
