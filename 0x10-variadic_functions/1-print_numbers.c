130b5b5abf0702f7d886a528baeaa742b259dd6d
alahiane <ahmedpzzz24@gmail.com>
2023-10-12 13:42:03 -0700

    azer

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i == n - 1)
 			printf("%d", va_arg(num, int));
 		else
-			printf("%d%s", va_arg(num, int), *separator);
+			printf("%d%s", va_arg(num, int), separator);
 		i++;
 	}
 	printf("\n");
